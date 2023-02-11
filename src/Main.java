5084115e5cadb0b69b575ee8e016b5f5bb2ca877
r-ikemori <dragon06051996@gmail.com>
2023-02-11 16:46:10 +0900

    カプセル化

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
-import work.comment.Zenhan;
-
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		// TODO 自動生成されたメソッド・スタブ
-		Zenhan.doWarusa();
-		Zenhan.doTOgame();
-		work.comment.kouhan.callDese();
-		work.comment.kouhan.showMondokoro();
-
-
+		System.out.println("hello");
 	}
 
 }
